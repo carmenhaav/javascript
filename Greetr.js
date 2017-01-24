5cfbc16616c8c037994fd95890b117071593b31a
carmenhaav <carmen.haav@khk.ee>
2017-01-24 19:04:16 +0200

    esimene kommentaar Greetr.js failis

--- Greetr.js
+++ Greetr.js
@@ -1,3 +1,9 @@
 /**
  * Created by carmenhaav on 23/01/2017.
  */
+// luuakse function literal, mida ei kutsuta välja, kõik, mis jääb/läheb sisse - toimib koheselt
+(function(global, $) {
+
+
+
+}(window, jQuery));
