75c3f41a68c6b943777983b20b70a5ae258d21fb
carmenhaav <carmen.haav@khk.ee>
2017-01-24 19:33:58 +0200

    kolmanda osa kommentaarid Greetr.js failis

--- Greetr.js
+++ Greetr.js
@@ -9,8 +9,90 @@
         return new Greetr.init(firstName, lastName, language);
     }
 
+    // pole kättesaadav, see on peidus IIFE skoobi ahelas
+    var supportedLangs = ['en', 'es'];
+
+    //  keelte tervitustega objekt
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    // keelte tervitustega formaalne objekt
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    // sisse logitud sõnum
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inició sesión'
+    };
+
     // luuakse prototüüp (sisse tulevad meetodid, mis vähendavad koodi mahukust)
-    Greetr.prototype = {};
+    Greetr.prototype = {
+
+        // 'this' viitab välja kutsutavale objektile sooritamise ajal
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        // tingimuslause, kui keel on invalid
+        validate: function() {
+            if (supportedLangs.indexOf(this.language)  === -1) {
+                throw "Invalid language";
+            }
+        },
+
+        // sõnumitagastus
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        // sõnumitagastus
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+
+        // ahelmeetodid, tagastavad enda objekti
+        greet: function(formal) {
+            var msg;
+
+            // tingimuslaused, kui väärtus on undefined või null, siis 'surutakse' valeks
+            if (formal) {
+                msg = this.formalGreeting();
+            }
+            else {
+                msg = this.greeting();
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            // teeb meetodi ahelaks
+            return this;
+        },
+
+
+        log: function() {
+            if (console) {
+                console.log(logMessages[this.language] + ': ' + this.fullName());
+            }
+
+            return this;
+        },
+
+        // määrab keele ja valideerib selle
+        setLang: function(lang) {
+            this.language = lang;
+
+            this.validate();
+
+            return this;
+        }
+    };
 
     // luuakse Greetr objekt, mis lubab teha uusi objekte, kasutamata 'new' võtmesõna/operaatorit
     Greetr.init = function(firstName, lastName, language) {
@@ -25,7 +107,7 @@
     // koodilühendus, ei pea kirjutama 'new' iga kord
     Greetr.init.prototype = Greetr.prototype;
 
-    // Greetr objekt seotakse globaalse keskkonnaga ja Greetr lühendatakse $G, et koodi/aja kokkuhoiuks
+    // Greetr objekt seotakse globaalse keskkonnaga ja Greetr lühendatakse $G-ga koodi/aja kokkuhoiuks
     global.Greetr = global.G$ = Greetr;
 
 
